92d5824348b2b37b4831831f47b3b3734dd8c1bf
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-02-20 16:16:53 +0200

    inceteev

---
+++ configs.js
@@ -0,0 +1,33 @@
+var _ = require("lodash");
+var fs= require("fs");
+var prompt = require("prompt-sync").prompt;
+var dist = JSON.parse(fs.readFileSync("app/config/config.json.dist").toString("UTF-8"));
+var config = {};
+try {
+  config = JSON.parse(fs.readFileSync("app/config/config.json").toString("UTF-8"));
+} catch (ex) {
+  console.log(ex);
+}
+var result = {}
+ _.assign(result, dist, config)
+walk(result, 0, "");
+//console.log(result);
+fs.writeFileSync("app/config/config.json", JSON.stringify(result, undefined, 4), "UTF-8");
+
+
+function walk(object, step, path) {
+  var answer;
+  for (var prop in object) {
+    if (typeof object[prop] === "object") {
+      walk(object[prop], 1 + step, path + prop + ".");
+    } else {
+        answer = ""
+        process.stdout.write(path +  prop + " : ");
+        answer = prompt({value: object[prop]});
+        if (answer) {
+          object[prop] = answer;
+        }
+        console.log(path +  prop + " = " + object[prop]);
+    }
+  }
+}
