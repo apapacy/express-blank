bda1e22a825bab81ae0a267e989751a0da817569
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-02-22 21:20:43 +0200

    async ++

--- app/async.js
+++ app/async.js
@@ -1,17 +1,9 @@
-function testPromise(message, timeout) {
-  return new Promise(function(ok, ex) {
-    setTimeout(function() {
-      ok(message);
-    }, timeout);
-  });
-}
-async.stack = []
 function* async(promise) {
-  var it = async.stack.pop();
+  var iter = async.stack.pop();
   promise.then(function(value) {
-    async.stack.push(it)
-    var next = it.next(value);
-    if (next.done) {
+    async.stack.push(iter);
+    var next = iter.next(value);
+    if (!next.done) {
       async.stack.pop();
     }
   });
@@ -24,39 +16,8 @@ function run(self, func, args) {
   iter.next();
 }
 
-function* main(message) {
-  var value = yield * async(testPromise(message, 3000))
-  console.log("*****" + value);
-  console.log("after first promise");
-  var value = yield * async(testPromise("2nextMessage", 3000))
-  console.log("*****" + value);
-  console.log("after 2first promise");
-  var value = yield * async(testPromise("3nextMessage", 3000))
-  console.log("*****" + value);
-  console.log("after 3first promise");
-  var value = yield * async(testPromise("4nextMessage", 3000))
-  console.log("*****" + value);
-  console.log("after 4first promise");
-  var value = yield * async(testPromise("5nextMessage", 3000))
-  console.log("*****" + value);
-  console.log("after 5first promise");
-  var value = yield * async(testPromise("6nextMessage", 3000))
-  console.log("*****" + value);
-  console.log("after 6first promise");
-  var value = yield * async(testPromise("7nextMessage", 3000))
-  console.log("*****" + value);
-  console.log("after 7first promise");
-  var value = yield * async(testPromise("8nextMessage", 3000))
-    console.log(value);
-}
+async.run = run;
 
-run(null, main, ["restify"]);
+async.stack = []
 
-function* test(){
-  var test = yield 16;
-  console.log("test: " + test)
-}
-
-//var it = test(18);
-//console.log(it.next())
-//console.log(it.next(20))
+module.exports = async;
